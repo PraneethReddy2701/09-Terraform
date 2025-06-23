f431ac02284a9714a4c78bfb0a2860471e3b70db
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-23 21:21:23 +0530

    terraform practice security group creation

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "This security group allows traffic from all ports"
+
+  tags = {
+    Name = "allow_all"
+  }
+
+
+ ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+ egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
