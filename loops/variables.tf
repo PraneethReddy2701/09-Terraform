f8e8fb0ae368e262e6c701f330055b9d83ca4f88
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-07-07 14:29:19 +0530

    count based loop

--- loops/variables.tf
+++ loops/variables.tf
@@ -1,49 +1,49 @@
-variable "ami_id"{
-    type    = string
-    default = "ami-09c813fb71547fc4f"
-    description = "This is our daily used AMI"
+variable "ami_id" {
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "This is our daily used AMI"
 }
 
 variable "instances" {
-    default = ["mongodb", "redis", "mysql", "rabbitmq"]
-  
+  default = ["mongodb", "redis", "mysql", "rabbitmq"]
+
 }
 variable "environment" {
-    default = "dev" 
+  default = "dev"
 }
 
 variable "sg_name" {
-    default = "allow_all"
+  default = "allow_all"
 }
 
 variable "sg_description" {
-    default = "Allowing all ports from internet"
+  default = "Allowing all ports from internet"
 }
 
 variable "from_port" {
-    type = number
-    default = 0
+  type    = number
+  default = 0
 }
 
 variable "to_port" {
-    type = number
-    default = 0
+  type    = number
+  default = 0
 }
 
 variable "cidr_blocks" {
-    default = ["0.0.0.0/0"]
+  default = ["0.0.0.0/0"]
 }
 
 variable "sg_tags" {
-    default = {
-        Name = "allow-all"
-    } 
+  default = {
+    Name = "allow-all"
+  }
 }
 
 variable "zone_id" {
-    default = "Z0057140DV55FUG37SFU"  
+  default = "Z0057140DV55FUG37SFU"
 }
 
 variable "domain_name" {
-    default = "bittu27.site"
+  default = "bittu27.site"
 }
